6e63c5c79d532d9e4b7e76448b7b89eca00791cf
Ramesh Subramonian <subramonian@gmail.com>
2019-06-28 18:36:16 +0000

    ok

--- OPERATORS/MDB/lua/mk_comp_key_val.lua
+++ OPERATORS/MDB/lua/mk_comp_key_val.lua
@@ -19,6 +19,9 @@ local function mk_comp_key_val(Tk, val_vec)
 
   -- START: Basic checks on input 
   nDR, in_vecs = get_nDR(Tk)
+  for i = 1, nD do 
+    assert(type(in_vecs[i] == "lVector"))
+  end
   -- currently only one value can be aggregated
   assert(type(val_vec) == "lVector") 
   local val_type = val_vec:fldtype()
@@ -68,65 +71,82 @@ mk_comp_key_val_F4(
   end
   --===================================================================
   -- START Allocate necessary buffers
-  local val_buf  -- allocated on first call, not before
-  local key_buf  -- allocated on first call, not before
-  local chunk_idx = 0
-  local first_call = true
   --==============================================
   local key_type = "I8" -- HARD CODED
   local key_cast_as = "uint64_t * " -- HARD CODED
   local key_width = qconsts.qtypes[key_type].width
+  local key_buf = assert(ffi.cast(key_cast_as,
+          get_ptr(cmem.new(nK * key_width, key_type))))
 
   local val_ctype = qconsts.qtypes[val_type].ctype
   local val_cast_as = val_ctype .. " * "
   local val_width = qconsts.qtypes[val_type].width
+  local val_buf = assert(ffi.cast(val_cast_as, 
+          get_ptr(cmem.new(nK * val_width, val_type))))
 
   local nK = qconsts.chunk_size
   --==============================================
   local in_dim_vals = ffi.cast("uint8_t **",
     get_ptr(cmem.new(nD * ffi.sizeof("uint8_t *"))))
-
+  --==============================================
+  -- Unlike usual pattern, we are pre-allocating buffers
+  -- Usually, we wait for first call to do so 
+  local chunk_idx = 0
   --===================================================================
--- TODO  key_vec = lVector( {gen = true, has_nulls = false, qtype = key_type})
-  val_vec = lVector( {gen = true, has_nulls = false, qtype = val_type})
-  --===================================================================
-  function kv_gen(chunk_num)
-    assert(chunk_num == chunk_idx)
-    if ( first_call ) then
-      first_call = false
-      key_buf = assert(ffi.cast(key_cast_as,
-        get_ptr(cmem.new(nK * key_width, key_type))))
-      val_buf = assert(ffi.cast(val_cast_as, 
-        get_ptr(cmem.new(nK * val_width, val_type))))
+  local M = { "key", "val" }
+  lgens = {} -- we make a table of two generators 
+  for _, vecname in pairs(M) do 
+    local function kv_gen(chunk_num)
+      assert(chunk_num == chunk_idx)
+      local len   = {}
+      local chunk = {}
+      if ( 0 ) then -- you need to consume more stuff ) then 
+        for i = 1, nD do 
+          len[i], chunk[i] = in_vecs[i]:chunk(chunk_idx)
+        end
+      end
+      -- Check that all input vectors behave similarly
+      for i = 2, nD do 
+        assert(len[i] == len[1] )
+        if ( chunk[1] == nil ) then assert(chunk[i] == nil) end 
+        if ( chunk[1] ~= nil ) then assert(chunk[i] ~= nil) end 
+      end
+      --==============================
+      if ( len[1] == 0 ) then 
+        if ( vecname == "key" ) then 
+          key_vec:eov()
+        elseif ( vecname == "val" ) then 
+          val_vec:eov()
+        end
+        return 0, nil
+      end
+      --==============
+      -- Cast input the way C needs it 
+      for i = 1, nD do 
+        in_dim_vals[i-1] = ffi.cast("uint8_t *", get_ptr(chunk[i]))
+      end
+      -- TODO Need to adjust nV
+      local nV = qconsts.chunk_size
+      local start_time = qc.RDTSC()
+      local status = fnptr(template, nR, nC, in_dim_vals, in_measure_val, 
+       key_buf, val_buf, nV, nK)
+      assert(status == 0)
+      record_time(start_time, func_name)
+      -- here's a bit of trickery needed to handle the case that the 
+      -- vectors are produced at the same time and are not independent
+      if ( vecname == "key" ) then 
+        val_vec:put_chunk(val, nil, num_kvs_produced)
+        return num_kvs_produced, key_buf
+      elseif ( vecname == "val" ) then 
+        key_vec:put_chunk(val, nil, num_kvs_produced)
+        return num_kvs_produced, val_buf
+      end
+      --===========================
     end
-    local len   = {}
-    local chunk = {}
-    for i = 1, nD do 
-      assert(type(in_vecs[i] == "lVector"))
-      -- len[i], chunk[i] = in_vecs[i]:chunk(chunk_idx)
-      print(i, nD) -- , in_vecs[i]:fldtype())
-      local x, y = in_vecs[i]:chunk(chunk_idx)
-    end
-    for i = 2, nD do 
-      assert(len[i] == len[1] )
-      if ( chunk[1] == nil ) then assert(chunk[i] == nil) end 
-      if ( chunk[1] ~= nil ) then assert(chunk[i] ~= nil) end 
-    end
-    if ( len[1] == 0 ) then return XX, YY end
-    --==============
-    for i = 1, nD do 
-      in_dim_vals[i-1] = ffi.cast("uint8_t *", get_ptr(chunk[i]))
-    end
-    -- TODO Need to adjust nV
-    local nV = qconsts.chunk_size
-    local start_time = qc.RDTSC()
-    local status = fnptr(template, nR, nC, in_dim_vals, in_measure_val, 
-     key_buf, val_buf, nV, nK)
-    assert(status == 0)
-    record_time(start_time, func_name)
-    return XX, YY
+    lgens[vecname] = kvgen
   end
-  key_vec = lVector( {gen = kv_gen, has_nulls = false, qtype = key_type})
+  key_vec = lVector( {gen = gens.key, has_nulls = false, qtype = key_type})
+  val_vec = lVector( {gen = gens.val, has_nulls = false, qtype = val_type})
   return key_vec, val_vec
 end
 -- return mdb -- FOR UNIT TESTING
