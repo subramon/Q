217ca55eb7663aa47d57a1874706e9a9a28d28b4
Ramesh Subramonian <subramonian@gmail.com>
2019-08-02 14:30:56 +0000

    ok

--- RUNTIME/test/gc_foo.lua
+++ RUNTIME/test/gc_foo.lua
@@ -1,26 +1,24 @@
 -- following hard coded here for now 
-local qconsts = require 'Q/UTILS/lua/q_consts'
-local ffi     = require 'Q/UTILS/lua/q_ffi'
 local get_hdr = require 'Q/UTILS/lua/get_hdr'
-local qc      = require 'Q/UTILS/lua/q_core'
 local cmem = require 'libcmem'
---=======================================
 local get_ptr = require 'Q/UTILS/lua/get_ptr'
+
 
 local function gc_foo(n)
   local x = assert(cmem.new(1048576, "I4", "remainder"))
   x:zero()
- -- THIS CAUSES A PROBLEM    local c_x = get_ptr(x, "I4")
+  local c_x = get_ptr(x, "I4")
+
 
   local out = {}
   local iter = iter or n
   local function setter()
-    local c_x = get_ptr(x, "I4")
+    if ( false ) then c_x = get_ptr(x, "I4") end -- comment to force bug
     c_x[0] = iter
     iter = iter + 1
   end
   local function getter()
-    local c_x = get_ptr(x, "I4")
+    if ( false ) then c_x = get_ptr(x, "I4") end -- comment to force bug
     return tonumber(c_x[0])
   end
   out.setter = setter
