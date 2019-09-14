343f5d074030d0a9ff708a769c653563de62c993
Ramesh Subramonian <subramonian@gmail.com>
2019-09-14 01:46:52 +0000

    ok

---
+++ RUNTIME/MAGG/inc/hmap_common.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 Ramesh Subramonian <subramonian@gmail.com>
+ * All rights reserved.
+ *
+ * Use is subject to license terms, as specified in the LICENSE file.
+ */
+
+#ifndef _HMAP_H
+#define _HMAP_H
+
+#include <assert.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <omp.h>
+#include "macros.h"
+#include "fastdiv.h"
+
+#define	Q_RHM_SET  1
+#define	Q_RHM_ADD 2
+
+#define	HASH_INIT_SIZE		(65536)
+#define	MAX_GROWTH_STEP		(1024U * 1024)
+
+#define	LOW_WATER_MARK 0.4
+#define	HIGH_WATER_MARK 0.85
+
+#define RH_CHUNK_SIZE 1024
+
+#endif
