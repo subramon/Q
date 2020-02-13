68c55ceef6667598034c3ae251ea5924cdf4cf11
Ramesh Subramonian <subramonian@gmail.com>
2020-02-13 05:10:10 +0000

    ok

---
+++ RUNTIME/VCTR/inc/struct_timers.h
@@ -0,0 +1,24 @@
+#ifndef _STRUCT_TIMERS 
+#define _STRUCT_TIMERS 
+ typedef struct _vec_timers_type { 
+uint64_t t_check ; uint32_t n_check ;
+uint64_t t_clone ; uint32_t n_clone ;
+uint64_t t_delete_chunk_file ; uint32_t n_delete_chunk_file ;
+uint64_t t_delete_master_file ; uint32_t n_delete_master_file ;
+uint64_t t_flush ; uint32_t n_flush ;
+uint64_t t_free ; uint32_t n_free ;
+uint64_t t_get1 ; uint32_t n_get1 ;
+uint64_t t_start_read ; uint32_t n_start_read ;
+uint64_t t_get_chunk ; uint32_t n_get_chunk ;
+uint64_t t_new ; uint32_t n_new ;
+uint64_t t_put1 ; uint32_t n_put1 ;
+uint64_t t_put_chunk ; uint32_t n_put_chunk ;
+uint64_t t_rehydrate_single ; uint32_t n_rehydrate_single ;
+uint64_t t_rehydrate_multi ; uint32_t n_rehydrate_multi ;
+uint64_t t_shutdown ; uint32_t n_shutdown ;
+uint64_t t_start_write ; uint32_t n_start_write ;
+uint64_t t_malloc ; uint32_t n_malloc ;
+uint64_t t_memcpy ; uint32_t n_memcpy ;
+uint64_t t_memset ; uint32_t n_memset ;
+ } VEC_TIMERS_TYPE; 
+#endif 
---
+++ RUNTIME/VCTR/src/reset_timers.c
@@ -0,0 +1,22 @@
+#include "struct_timers.h"
+void reset_timers( VEC_TIMERS_TYPE *ptr_T) { 
+ptr_T->t_check  = 0 ; ptr_T->n_check  = 0 ; 
+ptr_T->t_clone  = 0 ; ptr_T->n_clone  = 0 ; 
+ptr_T->t_delete_chunk_file  = 0 ; ptr_T->n_delete_chunk_file  = 0 ; 
+ptr_T->t_delete_master_file  = 0 ; ptr_T->n_delete_master_file  = 0 ; 
+ptr_T->t_flush  = 0 ; ptr_T->n_flush  = 0 ; 
+ptr_T->t_free  = 0 ; ptr_T->n_free  = 0 ; 
+ptr_T->t_get1  = 0 ; ptr_T->n_get1  = 0 ; 
+ptr_T->t_start_read  = 0 ; ptr_T->n_start_read  = 0 ; 
+ptr_T->t_get_chunk  = 0 ; ptr_T->n_get_chunk  = 0 ; 
+ptr_T->t_new  = 0 ; ptr_T->n_new  = 0 ; 
+ptr_T->t_put1  = 0 ; ptr_T->n_put1  = 0 ; 
+ptr_T->t_put_chunk  = 0 ; ptr_T->n_put_chunk  = 0 ; 
+ptr_T->t_rehydrate_single  = 0 ; ptr_T->n_rehydrate_single  = 0 ; 
+ptr_T->t_rehydrate_multi  = 0 ; ptr_T->n_rehydrate_multi  = 0 ; 
+ptr_T->t_shutdown  = 0 ; ptr_T->n_shutdown  = 0 ; 
+ptr_T->t_start_write  = 0 ; ptr_T->n_start_write  = 0 ; 
+ptr_T->t_malloc  = 0 ; ptr_T->n_malloc  = 0 ; 
+ptr_T->t_memcpy  = 0 ; ptr_T->n_memcpy  = 0 ; 
+ptr_T->t_memset  = 0 ; ptr_T->n_memset  = 0 ; 
+}
