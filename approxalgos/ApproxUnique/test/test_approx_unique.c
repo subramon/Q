377050b7f3489da5819741fd535a90284d86b8a1
Ramesh Subramonian <subramonian@gmail.com>
2020-03-18 22:47:16 +0000

    ok

--- approxalgos/ApproxUnique/test/test_approx_unique.c
+++ approxalgos/ApproxUnique/test/test_approx_unique.c
@@ -16,39 +16,24 @@ main(
   approx_unique_state_t state;
   double accuracy; int estimate; int is_good;
   int n = 1048576;
-  int niters = 2;
-  int val;
-  for ( int iter = 0; iter < niters; iter++ ) { 
-  status = approx_unique_make(&state, 0); cBYE(status);
-  switch ( iter ) { 
-    case 0 : /* all distinct values */
-      val = 0;
-      for ( int i = 0; i < n; i++ ) { 
-        status = approx_unique_exec(&state, (char *)&val, sizeof(int)); 
-        cBYE(status);
-        val++;
-      }
-      break;
-    case 1 : 
-      //-- Case 2: half are unique values */
-      val = 0;
-      for ( int i = 0; i < n; i++ ) { 
-        status = approx_unique_exec(&state, (char *)&val, sizeof(int)); 
-        cBYE(status);
-        val++;
-        if ( val == n/2 ) { val = 0; }
-      }
-      break;
-    default : 
-      go_BYE(-1);
-      break;
-  }
-
-  status = approx_unique_final(&state, &estimate, &accuracy, &is_good);
-  printf("estimate = %d \n", estimate);
-  printf("accuracy = %f \n", accuracy);
-  printf("is_good  = %d \n", is_good);
-  status = approx_unique_free(&state); cBYE(status);
+  int niters = 12;
+  int denom = 1; 
+  for ( int iter = 0; iter < niters; iter++ ) {
+    int max_val = n  / denom;
+    status = approx_unique_make(&state, 0); cBYE(status);
+    int val = 0;
+    for ( int i = 0; i < n; i++ ) { 
+      status = approx_unique_exec(&state, (char *)&val, sizeof(int)); 
+      cBYE(status);
+      val++;
+      if ( val == max_val ) { val = 0; } 
+    }
+    status = approx_unique_final(&state, &estimate, &accuracy, &is_good);
+    printf("estimate = %d \n", estimate);
+    printf("accuracy = %f \n", accuracy);
+    printf("is_good  = %d \n", is_good);
+    status = approx_unique_free(&state); cBYE(status);
+    denom = denom * 2;
   }
 BYE:
   return status;
