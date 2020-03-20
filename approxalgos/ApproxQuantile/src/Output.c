c6ae48ad2a4126b74660539b229969451e6a01ee
Ramesh Subramonian <subramonian@gmail.com>
2020-03-20 18:03:17 +0000

    ok

--- approxalgos/ApproxQuantile/src/Output.c
+++ approxalgos/ApproxQuantile/src/Output.c
@@ -12,33 +12,52 @@ Output(
        int b,         
        int k
        )
-// STOP FUNC DECL
-//---------------------------------------------------------------------------
+//-------------------------------------------------------------------------
 /* README: 
 
-Output(buffer,weight,last_packet,last_packet_size,siz,num_quantiles,quantiles,b,k): This function takes as input the 2d buffer array, the weight array and the last packet and answers quantile queries.
+Output(buffer,weight,last_packet,last_packet_size,siz,num_quantiles,quantiles,b,k): 
+
+This function takes as input the 2d buffer array, the weight array and
+the last packet and answers quantile queries.
 
 NOTE: you need to ensure the last packet is sorted beforehand
 
-NOTE:  Consider the following way of viewing the buffer array: each element in a buffer occurs as many times as it's corresponding weight in the weight array. When calling this function, the total number of "effective" entries in the 2d buffer array and the last packet put together is exactly siz. 
+NOTE: Consider the following way of viewing the buffer array: each
+element in a buffer occurs as many times as it's corresponding weight
+in the weight array. When calling this function, the total number of
+"effective" entries in the 2d buffer array and the last packet put
+together is exactly siz.
 
-For example: Consider the Munro-Patterson algorithm. New() adds k "effective" elements to the 2d buffer array for every k elements in the input data. Collapse() doesn't alter the number of effective elements in the 2d buffer array. Hence at this stage, the total number of "effective" elements is equal to the total number of elements in the input data.
+For example: Consider the Munro-Patterson algorithm. New() adds k
+"effective" elements to the 2d buffer array for every k elements in
+the input data. Collapse() doesn't alter the number of effective
+elements in the 2d buffer array. Hence at this stage, the total number
+of "effective" elements is equal to the total number of elements in
+the input data.
 
 INPUTS:
 
 buffer: The 2d buffer array containing the b buffers of size k each
 
-weight: Weight array which contains the weight information of each buffer in the 2d buffer array
+weight: Weight array which contains the weight information of each
+buffer in the 2d buffer array
 
 last_packet: Array containing the last packet 
 
-last_packet_incomplete: if the last packet buffer is non-null, then this will be 1. if the last packet in the input data is handled by inputBuffer itself (this will happen when k (or the number of non-zero entries in cfld) divides siz), this parameter will be 0, meaning last_packet buffer is not necessary.
+last_packet_incomplete: if the last packet buffer is non-null, then
+this will be 1. if the last packet in the input data is handled by
+inputBuffer itself (this will happen when k (or the number of non-zero
+entries in cfld) divides siz), this parameter will be 0, meaning
+last_packet buffer is not necessary.
 
 last_packet_size: Size of the last packet (less than or equal to k)
 
 siz: Size of the total data passed to the approx_quantile function
 
-num_quantiles: Total number of quantiles requested. For ex: num_quantiles = 100 implies you need quantile queries every 1% from 0% to 100%, num_quantiles = 200 implies you need quantile queries every 0.5% from 0 to 100% and so on.
+num_quantiles: Total number of quantiles requested. For ex:
+num_quantiles = 100 implies you need quantile queries every 1% from 0%
+to 100%, num_quantiles = 200 implies you need quantile queries every
+0.5% from 0 to 100% and so on.
 
 b: Number of buffers in the 2d buffer array (produced by determine_b_k)
 
@@ -49,16 +68,17 @@ OUTPUTS:
 
 quantiles: Array where the calculated quantiles are going to be stored. 
 
-NOTE: This function is slightly different from the paper, in order to take into consideration the last packet 
+NOTE: This function is slightly different from the paper, in order to
+take into consideration the last packet
 
 */
-//---------------------------------------------------------------------------
+//--------------------------------------
 {
   int status = 0;
   int * current_loc_buf = NULL;
 
   int last_packet_incomplete;
-  if ( last_packet_size == 0 ) { 
+  if ( last_packet_size == 0 ) {  // TODO THINK
     last_packet_incomplete = 1;  
   }
   else {
@@ -71,9 +91,21 @@ NOTE: This function is slightly different from the paper, in order to take into 
   if ( quantiles == NULL ) { go_BYE(-1); }
   if ( last_packet_size > k ) { go_BYE(-1); }
 
-  /* To calculate the quantiles from the 2d buffer array and the last array (which have siz number of "effective" elements, we need to merge and  sort them first and also keep track of their corresponding weights. Once the merging and sorting is done, we can just do a sequential scan and look at each element as if it is occuring as many times as its corresponding weight ("effective" number of times) and answer all the quantile queries.
+  /* To calculate the quantiles from the 2d buffer array and the last
+     array (which have siz number of "effective" elements, we need to
+     merge and sort them first and also keep track of their
+     corresponding weights. Once the merging and sorting is done, we
+     can just do a sequential scan and look at each element as if it
+     is occuring as many times as its corresponding weight
+     ("effective" number of times) and answer all the quantile
+     queries.
 
-     This merging and sorting can be done by using a temporary 1d array of size (b+1)*k and another array of same size to keep track of their corresponding weights, but that's a waste of space. It can be done intelligently without using any additional memory by using counters current_loc_buf[b] ( and current_loc_lp ), current_quantile_rank and current_element_rank.
+     This merging and sorting can be done by using a temporary 1d
+     array of size (b+1)*k and another array of same size to keep
+     track of their corresponding weights, but that's a waste of
+     space. It can be done intelligently without using any additional
+     memory by using counters current_loc_buf[b] ( and current_loc_lp
+     ), current_quantile_rank and current_element_rank.
 
 NOTE: all arrays considered here are presorted */
 
@@ -82,15 +114,21 @@ NOTE: all arrays considered here are presorted */
   memset(current_loc_buf, 0,  b * sizeof(int)); 
   /* keeps track of the current processing location in each buffer */
   int current_loc_lp = 0; 
-  /* keeps track of current processing location in the last packet if it is incomplete */
+  /* keeps track of current processing location in the last packet if
+     it is incomplete */
 
   int current_proc_element_rank = 0; 
-  /* keeps track of the rank of the current element being processed, i.e., how many "effective" elements have  been processed */
+  /* keeps track of the rank of the current element being processed,
+     i.e., how many "effective" elements have been processed */
 
   int jj = 0; 
-  /* keeps track of estimated number of quantiles, iterates from 0 to num_quantile-1 */
+  /* keeps track of estimated number of quantiles, iterates from 0 to
+     num_quantile-1 */
   int next_quantile_rank = (long long)ceil((jj+1)*(double)siz/num_quantiles); 
-  /* keeps track of the next quantile's rank to be estimated. ex: if next quantile to be estimated is 50%, then it would be siz/2 (it iterates from 1*siz/num_quantile to siz in steps of siz/num_quantile basically) */
+  /* keeps track of the next quantile's rank to be estimated. ex: if
+     next quantile to be estimated is 50%, then it would be siz/2 (it
+     iterates from 1*siz/num_quantile to siz in steps of
+     siz/num_quantile basically) */
 
 
   while ( true ) {
@@ -98,7 +136,9 @@ NOTE: all arrays considered here are presorted */
     int min_unproc_val = 0x7FFFFFFF; 
     /* stores the minimum value that has not been processed yet */
     int min_unproc_val_loc = -1; 
-    /* stores the buffer id (between 0 and b-1) if the minimum unprocessed value is in one of those buffers, stores b if it is in the last packet */
+    /* stores the buffer id (between 0 and b-1) if the minimum
+       unprocessed value is in one of those buffers, stores b if it is
+       in the last packet */
 
     for ( int ii = 0; ii < b; ii++ ) {
 
--- approxalgos/ApproxQuantile/test/test_approx_quantile.c
+++ approxalgos/ApproxQuantile/test/test_approx_quantile.c
@@ -11,56 +11,24 @@ main(
     char **argv
     )
 {
-#ifdef XXX
   int status = 0;
+  int nQ = 10;
+  uint64_t n = 1048576;
+  approx_quantile_state_t state;
+  double eps = 0.1;
+  int error_code;
 
-  int *x = NULL;
-  char *X = NULL; size_t nX = 0;
-
-  int * y = NULL;
-  double *quantile_percent = NULL;
-
-  // Generate data 
-  //----------------------------------------------------------------------
-
-  long long num_quantile = 1000; 
-  /* ex: 5 => need 5 quantiles {20%,40%,60%,80%,100%}, choose between 1 & number of elements to be considered */
-  if ( num_quantile == 0 ) { cBYE(-1);}
-
-  long long y_siz = num_quantile;
-  y = (int *)malloc( y_siz * sizeof(int) );
-  return_if_malloc_failed(y);
- 
-  double eps = 0.001;   /* 0.0001: Ok with +/- 0.01% error in quantile estimations. For ex: 50% -> ok with 50 +/- 0.01 % */ 
-  int estimate_is_good; 
-
-  status = approx_quantile(x, cfld, siz, num_quantile, eps, y, y_siz, &estimate_is_good); cBYE(status);
-
-  quantile_percent = (double *)malloc ( num_quantile * sizeof(double) );
-  return_if_malloc_failed(quantile_percent);
-
-  for ( int ii = 0; ii < num_quantile; ii++ ) { 
-    quantile_percent[ii] = (double)(ii+1)*100/num_quantile; 
-  }
- 
-  if ( estimate_is_good == 1 ) {
-
-    /* approx_quantile() succeeded in doing the quantile calculations. printing values */
-    for ( int ii = 0; ii < num_quantile; ii++ ) {
-      printf("%.2f%%: " ANSI_COLOR_GREEN"%d"ANSI_COLOR_RESET"\n", quantile_percent[ii],y[ii]);
+  cBYE(status);
+  for ( int denom = 1; denom <= 1024; denom *= 2 ) { 
+    status = approx_quantile_make( nQ, n, &state, eps, &error_code);
+    for ( int i = 0; i < 1048576; i++ ) { 
+      status = approx_quantile_add(&state, (double)i); cBYE(status);
     }
-
+    status = approx_quantile_final(&state); cBYE(status);
+    for ( int i = 0; i < state.nQ; i++ ) { 
+      fprintf(stdout, "%4d:%d:%ld\n", denom, i, state.quantiles[i]);
+    }
+    status = approx_quantile_free(&state); cBYE(status);
   }
-  else {
-
-    /* approx_quantile() failed in doing the calculations */
-    printf("\n Need too much memory. \n(i) If you are sure you can use more than 800 MB of memory, go and increase MAX_SZ in the approx_quantile() function. \n(ii) Else, just increase eps (approximation) and retry.  \n");
-
-  }
-  
 BYE:
-  free_if_non_null(y);
-  free_if_non_null(quantile_percent);
-  return(status);
-#endif
-}
+  return status;
