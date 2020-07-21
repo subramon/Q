bd7487bae007f59dc5c2e1acacca37b2a1ed22c5
Ramesh Subramonian <subramon@li262-157.members.linode.com>
2020-07-21 22:13:18 +0000

    ok

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ unset LUA_INIT
 unset LD_LIBRARY_PATH
 unset QC_FLAGS
 unset Q_DATA_DIR
-unset Q_TRACE_DIR
-unset Q_TMPL_DIR
-unset Q_BUILD_DIR
 # TODO fix bug with ld library path
 unset LD_LIBRARY_PATH
 
@@ -39,23 +36,6 @@ export Q_DATA_DIR="${Q_DATA_DIR:=${Q_ROOT}/data}"
 echo "Q_DATA_DIR: $Q_DATA_DIR"
 mkdir -p $Q_ROOT/meta
 
-mkdir -p $Q_ROOT/trace
-export Q_TRACE_DIR="${Q_TRACE_DIR:=${Q_ROOT}/trace}"
-echo "Q_TRACE_DIR: $Q_TRACE_DIR"
-
-mkdir -p $Q_ROOT/tmpl
-export Q_TMPL_DIR="${Q_TMPL_DIR:=${Q_ROOT}/tmpl}"
-echo "Q_TMPL_DIR: $Q_TMPL_DIR"
-
-export Q_BUILD_DIR="/tmp/q" # will figure out a better location later
-# Setting ld library path based on lua init
-#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$Q_ROOT/lib"
-# set +m
-# lua -e "print('hey')" &>/dev/null
-# RET=`echo $?`
-# if [[ "$RET" -ne "0" ]]; then
-#     `lua| tail -1`
-# fi
 export LD_LIBRARY_PATH="${Q_ROOT}/lib:/usr/local/lib64:${LD_LIBRARY_PATH}"
 echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
 # export Q_LINK_FLAGS=" -shared -lpthread -lm -lgomp "
@@ -69,9 +49,5 @@ export LUA_CPATH="${Q_ROOT}/lib/?.so;;"
 cd $CURR_PATH
 echo "LUA_PATH: $LUA_PATH"
 echo "LUA_CPATH: $LUA_CPATH"
-echo "Q_BUILD_DIR: $Q_BUILD_DIR"
-mkdir -p $Q_BUILD_DIR
-mkdir -p $Q_BUILD_DIR/src/
-mkdir -p $Q_BUILD_DIR/include/
 cd $PREV_DIR
 cd $CUR_DIR 
