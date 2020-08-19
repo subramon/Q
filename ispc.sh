e369c2a2bbf0747a4d2c7449c2a256fb892b8412
Ramesh Subramonian <subramonian@gmail.com>
2020-08-19 17:29:34 +0000

    ok

--- ispc.sh
+++ ispc.sh
@@ -5,8 +5,10 @@ rm -rf /var/lib/apt/lists/*
 wget https://cmake.org/files/v3.15/cmake-3.15.0-Linux-x86_64.sh 
 mkdir -p /opt/cmake 
 sh cmake-3.15.0-Linux-x86_64.sh --prefix=/opt/cmake --skip-license 
-ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake && ln -s /opt/cmake/bin/cmake /usr/bin/cmake 
-ln -s /opt/cmake/bin/ctest /usr/local/bin/ctest && ln -s /opt/cmake/bin/ctest /usr/bin/ctest 
+ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake 
+ln -s /opt/cmake/bin/cmake /usr/bin/cmake 
+ln -s /opt/cmake/bin/ctest /usr/local/bin/ctest 
+ln -s /opt/cmake/bin/ctest /usr/bin/ctest 
 rm cmake-3.15.0-Linux-x86_64.sh
 echo "All done"
 
