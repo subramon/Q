46f4941681e1b4de513bd2046ef004390d1b05c1
Ramesh Subramonian <ramesh.subramonian@target.com>
2020-09-17 12:38:52 -0700

    ok

---
+++ DT/inc/incs.h
@@ -0,0 +1,28 @@
+#ifndef __Q_INCS
+#define __Q_INCS
+#include <alloca.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <float.h>
+#include <inttypes.h>
+#include <limits.h>
+// TODO P4 do not think this is needed:#include <malloc.h>
+#include <math.h>
+#include <memory.h>
+#include <omp.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+#include "macros.h"
+#endif
