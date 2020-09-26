9f8f63f46a9b3271eff24bd249ae00734620f546
Ramesh Subramonian <subramonian@gmail.com>
2020-09-26 20:57:44 +0000

    ok

--- DT/inc/constants.h
+++ DT/inc/constants.h
@@ -1,4 +1,15 @@
+#undef DEVELOPMENT
+#ifdef DEVELOPMENT
 #define MIN_LEAF_SIZE 32
 #define NUM_FEATURES  2
 #define NUM_INSTANCES 1024
 #define BUFSZ  3
+#endif
+
+#define PERF_TEST
+#ifdef PERF_TEST
+#define MIN_LEAF_SIZE 64
+#define NUM_FEATURES  128
+#define NUM_INSTANCES 1048576
+#define BUFSZ         1024
+#endif
