5b6308ab90fe22b7cc197e47216786b9fb370731
Ramesh Subramonian <subramonian@gmail.com>
2020-10-01 15:33:09 +0000

    ok

--- DT/src/reorder.isp.c
+++ DT/src/reorder.isp.c
@@ -1,4 +1,5 @@
-#define get_from(x) ( x >> 32 )
+#include "constants.h"
+#include "macros.h"
 export void
 reorder(
     uniform uint64 Yj[],
@@ -12,23 +13,40 @@ reorder(
     uniform uint32 ptr_ridx[]
     )
 {
-  uint32 lidx = ptr_lidx[0];
-  uint32 ridx = ptr_ridx[0];
+  int status = 0;
+  uniform uint32 lidx = *ptr_lidx;
+  uniform uint32 ridx = *ptr_ridx;
   /* start ispc */
-  for ( uint32 i = lb; i < ub; i++ ) { 
-    uint32 idx;
-    uint32 from_i = get_from(Yj[i]);
+  for ( uint32 i = lb; i < ub; i++ ) {
+    int pos, is_left, is_right;
+    uint32 from_i = Yj[i] >> 32; // get_from(Yj[i]);
     uint32 to_i   = to_split_j[from_i];
-    bool is_left = to_i < split_yidx;
-    if ( is_left ) { 
-      idx = lidx; 
-      tmpYj[lidx++] = Yj[i]; 
+    //-----------------------------------------
+    is_left = 0;
+    if ( to_i < split_yidx ) { // this data point went left
+      is_left = 1;
+      pos = lidx + exclusive_scan_add(is_left);
+      tmpYj[pos] = Yj[i];
+      to_j[from_i] = pos;
+      /* Following is an experiment based on ISPC documentation
+       * uniform float a[] = ...;
+       int index = ...;
+       float * ptr = &a[index];
+       *ptr = 1;
+       */
+      uint32 * ptr = &(to_j[from_i]);
+      *ptr = pos;
     }
-    else { // this data point went right
-      idx = ridx; 
-      tmpYj[ridx++] = Yj[i]; 
+    lidx += reduce_add(is_left);
+    //------------------------
+    is_right = ~is_left;
+    if ( is_right == 1 ) {  // this data point went right
+      pos = ridx + exclusive_scan_add(is_right);
+      tmpYj[pos] = Yj[i];
+      to_j[from_i] = pos;
     }
-    to_j[from_i] = idx;
+    ridx += reduce_add(is_right);
+    //------------------------
   }
   ptr_lidx[0] = lidx;
   ptr_ridx[0] = ridx;
