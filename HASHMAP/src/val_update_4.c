0e6ec21906ef67ecde3dcae952f334feacb3a297
Ramesh Subramonian <ramesh.subramonian@target.com>
2020-12-04 11:02:47 -0800

    ok

--- HASHMAP/src/val_update_4.c
+++ HASHMAP/src/val_update_4.c
@@ -1,11 +1,11 @@
 // update the value 
 #include "q_incs.h"
 #include "q_macros.h"
-#include "val_struct_1.h"
+#include "val_struct_4.h"
 #include "val_update.h"
 
-// extern int num_mallocs;
-// extern int num_frees;
+extern int num_mallocs;
+extern int num_frees;
 
 int
 val_update(
@@ -17,18 +17,21 @@ val_update(
 
   if ( in_ptr_dst_val == NULL ) { go_BYE(-1); }
   if ( in_ptr_src_val == NULL ) { go_BYE(-1); }
-  agg_val_t **ptr_dst_val = (agg_val_t **)in_ptr_dst_val;
-  val_t     **ptr_src_val = (    val_t **)in_ptr_src_val;
+  val_t    **ptr_dst_val = (   val_t **)in_ptr_dst_val;
+  in_val_t **ptr_src_val = (in_val_t **)in_ptr_src_val;
+  in_val_t src_val = *((in_val_t *)ptr_src_val);
 
-  // if ( *ptr_dst_val != NULL ) { num_frees++; }
-  free_if_non_null(*ptr_dst_val); 
-  val_t src_val = *((val_t *)ptr_src_val);
-  val_t *dst_val = malloc(sizeof(val_t));
-  // printf("malloc'd %" PRIu64 "\n", ((uint64_t *)dst_val)); 
-  // num_mallocs++;
-  return_if_malloc_failed(dst_val);
-  *dst_val = src_val; 
-  *ptr_dst_val = dst_val;
+  if ( *ptr_dst_val == NULL ) { 
+    *ptr_dst_val = malloc(sizeof(val_t));
+    return_if_malloc_failed(*ptr_dst_val);
+    memset(*ptr_dst_val, 0,  (sizeof(val_t)));
+    num_mallocs++;
+  }
+  val_t *dst_val = *ptr_dst_val;
+  dst_val[0].cnt++;
+  dst_val[0].sumval += src_val;
+  if ( dst_val[0].minval > src_val ) { dst_val[0].minval = src_val; }
+  if ( dst_val[0].maxval < src_val ) { dst_val[0].maxval = src_val; }
 BYE:
   return status;
 }
--- HASHMAP/test/test4.c
+++ HASHMAP/test/test4.c
@@ -11,27 +11,28 @@
 int num_frees; int num_mallocs;
 int
 main(
-    void
+    int argc,
+    char **argv
     )
 {
   int status = 0;
   num_frees = num_mallocs = 0; 
   int num_iterations = 1024 * 1024;
-  int num_items      = 1024;
-  agg_val_t *chk_agg = NULL;
+  uint32_t num_items      = 1024;
+  val_t *chk_agg = NULL;
 
+  if ( argc != 1 ) { go_BYE(-1); }
   hmap_t hmap; memset(&hmap, 0, sizeof(hmap_t));
   dbg_t dbg; memset(&dbg, 0, sizeof(dbg_t));
   hmap_config_t config; memset(&config, 0, sizeof(hmap_config_t));
   config.min_size = 32;
-  config.max_size = 8*config.min_size;
+  config.max_size = 2 * num_items;
   bool malloc_key = true;
   bool reset_called = false;
 
-  agg_val_t *chk_agg = NULL;
-  chk_agg = malloc(num_items * sizeof(agg_val_t));
+  chk_agg = malloc(num_items * sizeof(val_t));
   return_if_malloc_failed(chk_agg);
-  memset(chk_agg, 0,  (num_items * sizeof(agg_val_t)));
+  memset(chk_agg, 0,  (num_items * sizeof(val_t)));
   status = hmap_instantiate(&hmap, &config); cBYE(status);
 
   srand48(random());
@@ -40,35 +41,39 @@ main(
     in_val_t val = drand48(); 
     //-- START: independent calculation of aggregation
     if ( chk_agg[key].minval > val ) { chk_agg[key].minval = val; }
-    if ( chk_agg[key].minval < val ) { chk_agg[key].maxval = val; }
-    chk_agg[key].sumval = val;
+    if ( chk_agg[key].maxval < val ) { chk_agg[key].maxval = val; }
+    chk_agg[key].sumval += val;
     chk_agg[key].cnt++;
     //-- STOP : independent calculation of aggregation
     status = hmap_put(&hmap, &key, sizeof(int), malloc_key, &val, &dbg); 
     cBYE(status);
   }
   status = hmap_chk(&hmap, reset_called); cBYE(status); 
-  printf("Iter = %d, Probes = %" PRIu64 "\n",iter,(long)dbg.num_probes); 
   printf("occupancy = %d \n", hmap.nitems);
   printf("size      = %d \n", hmap.size);
 
   // now confirm that every item has the aggregated value that it should
-  for ( uint32_t i = 0; i < nitems; i++ ) {
+  for ( uint32_t i = 0; i < num_items; i++ ) {
     int key = i;
+    uint32_t where_found; bool is_found;
     val_t *ptr_chk_val = NULL;
     status = hmap_get(&hmap, &key, sizeof(int), (void **)&ptr_chk_val, 
           &is_found, &where_found, &dbg); 
     cBYE(status);
     //-- START: Check against indepdent calculation 
     if ( !is_found ) { go_BYE(-1); }
-    if ( ptr_chk_val[0].cnt != chk_agg[key].cnt ) { go_BYE(-1); }
+    if ( where_found >= hmap.size ) { go_BYE(-1); }
+    if ( ptr_chk_val[0].cnt != chk_agg[key].cnt ) { 
+      go_BYE(-1); }
     if ( ptr_chk_val[0].minval != chk_agg[key].minval ) { go_BYE(-1); }
-    if ( ptr_chk_val[0].maxval != chk_agg[key].maxval ) { go_BYE(-1); }
+    if ( ptr_chk_val[0].maxval != chk_agg[key].maxval ) { 
+      go_BYE(-1); }
     if ( ptr_chk_val[0].sumval != chk_agg[key].sumval ) { go_BYE(-1); }
     //-- START: Check against indepdent calculation 
     if ( ptr_chk_val == NULL ) { go_BYE(-1); }
   }
   hmap_destroy(&hmap);
+  fprintf(stdout, "Test %s completed successfully\n", argv[0]);
 BYE:
   hmap_destroy(&hmap);
   return status;
