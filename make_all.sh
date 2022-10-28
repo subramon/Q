6d9e855bc08160aa4ad4eee403039f2411590cce
Ramesh Subramonian <subramonian@gmail.com>
2022-10-27 21:02:47 -0700

    ok

--- make_all.sh
+++ make_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+test -d $Q_SRC_ROOT
+find $Q_SRC_ROOT -name "*.o" -print | xargs rm 
+find $Q_SRC_ROOT -name "*.so" -print | xargs rm 
+test -d $Q_ROOT
+find $Q_ROOT/csos/*.so -print | xargs rm 
 # Quick and dirty way of compiling. Need to improve this
 cd $Q_SRC_ROOT/TMPL_FIX_HASHMAP/src/; make clean && make
 cd $Q_SRC_ROOT/TMPL_FIX_HASHMAP/VCTR_HMAP/; make clean && make
