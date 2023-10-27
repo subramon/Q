6e29885f2a05e8ac4bfeac4a93f34622b0569e8c
Ramesh Subramonian <ramesh.subramonian@target.com>
2023-10-27 22:24:52 +0000

    ok

--- RUNTIME/CNTR/test/test1.lua
+++ RUNTIME/CNTR/test/test1.lua
@@ -45,7 +45,7 @@ tests.t1 = function(test_num)
   return C
 end
 tests.t2 = function ()
-  local C = tests.t1(1)
+  local C1 = tests.t1(1)
   local len = 2 * blksz + 3 
   local vecs = {}
   local p = 4
@@ -53,15 +53,22 @@ tests.t2 = function ()
   vecs[2] = Q.period({start=1, by=1, period=p, qtype="I4", len=len })
   local optargs = {}
   optargs.name = "clone_name"
-  local C2 = C:clone(vecs, optargs)
+  local C2 = C1:clone(vecs, optargs)
   assert(type(C2)    == "KeyCounter")
   assert(C2:nitems() == 0)
-  assert(C2:label()  == C:label())
+  assert(C2:label()  == C1:label())
   assert(C2:name()  == "clone_name")
   assert(C2:is_eor() == false)
+
+  assert(C1:nitems() == 1)
   C2:eval()
   assert(C2:is_eor() == true)
   assert(C2:nitems() == p)
+
+  assert(C1:nitems() == 1)
+  C1:eval()
+  assert(C1:is_eor() == true)
+  assert(C1:nitems() == 1)
   print("Test t2 successfully completed.")
 
 end
