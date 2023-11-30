c7f49a33bd7fd63003a739713d9bfdc9b94635db
Ramesh Subramonian <subramonian@gmail.com>
2023-11-30 23:32:44 +0000

    ok

--- OPERATORS/JOIN/lua/expander_join.lua
+++ OPERATORS/JOIN/lua/expander_join.lua
@@ -65,10 +65,9 @@ local function expander_join(
         nn_dv_bufs[join_type] = nn_dv_buf
       end
       --==========================================================
-      print("DST:   Getting " .. l_chunk_num)
       local dl_len, dl_buf = dst_lnk:get_chunk(l_chunk_num)
       dst_start[0] = 0
-      print("dl_len = ", dl_len)
+      print("DST:   Getting " .. l_chunk_num .. " of size " .. dl_len)
       if ( dl_len == 0 ) then
         if ( true ) then -- TODO P0 MAJOR HACK
           print("XXXXXX")
@@ -85,37 +84,71 @@ local function expander_join(
         end
         return 0  -- tell my vector that it is over 
       end
+      --==========================================================
       -- Use sl, sv, dl to populate dv
       -- need a while loop becuase may need to consume > 1 chunk of sl/sv
       -- to produce one chunk of dv 
-      local sv_len, sv_buf = src_val:get_chunk(src_chunk_num)
-      local sl_len, sl_buf = src_lnk:get_chunk(src_chunk_num)
-       print("SRC:   Getting " .. src_chunk_num)
-      -- basic tests 
-      assert(sl_len == sv_len)
-      if ( sv_buf ) then assert(sl_buf) end 
-      if ( not sv_buf ) then assert(not sl_buf) end 
-        --================================================
-      -- get pointers to sl, sv, dl
-      local sv_ptr = ffi.NULL; local sl_ptr = ffi.NULL;
-      if ( sl_len > 0 ) then 
-        sl_ptr  = ffi.cast(subs.src_lnk_cast_as, get_ptr(sl_buf))
-        sv_ptr  = ffi.cast(subs.src_val_cast_as, get_ptr(sv_buf))
-      end
-      local dl_ptr  = ffi.cast(subs.src_lnk_cast_as, get_ptr(dl_buf))
-        --======================================================
-      -- with the same values of sl, sv, dl we perform many different joins
-      for k, join_type in ipairs(join_types) do
-        local dv_ptr  = ffi.cast(subs.src_val_cast_as, 
-          get_ptr(dv_bufs[join_type]))
-        local nn_dv_ptr  = ffi.cast("bool *", 
-          get_ptr(nn_dv_bufs[join_type]))
-        local func_name = subs.fn
-        local status = qc[func_name](
-          sv_ptr, sl_ptr, src_start, sl_len, dl_ptr, dv_ptr, nn_dv_ptr, 
-          dst_start, dl_len)
+      local iter = 1 -- for debugging 
+      while true do  -- start while loop AA
+        print("Iteration " .. iter .. " for dst chunk ".. l_chunk_num)
+        local sv_len, sv_buf = src_val:get_chunk(src_chunk_num)
+        local sl_len, sl_buf = src_lnk:get_chunk(src_chunk_num)
+         print("SRC:   Getting " .. src_chunk_num)
+        -- basic tests 
+        assert(sl_len == sv_len)
+        if ( sv_buf ) then assert(sl_buf) end 
+        if ( not sv_buf ) then assert(not sl_buf) end 
+          --================================================
+        -- get pointers to sl, sv, dl
+        local sv_ptr = ffi.NULL; local sl_ptr = ffi.NULL;
+        if ( sl_len > 0 ) then 
+          sl_ptr  = ffi.cast(subs.src_lnk_cast_as, get_ptr(sl_buf))
+          sv_ptr  = ffi.cast(subs.src_val_cast_as, get_ptr(sv_buf))
+        end
+        local dl_ptr  = ffi.cast(subs.src_lnk_cast_as, get_ptr(dl_buf))
+          --======================================================
+        -- with the same values of sl, sv, dl we perform many different joins
+        for k, join_type in ipairs(join_types) do
+          local dv_ptr  = ffi.cast(subs.src_val_cast_as, 
+            get_ptr(dv_bufs[join_type]))
+          local nn_dv_ptr  = ffi.cast("bool *", 
+            get_ptr(nn_dv_bufs[join_type]))
+          local func_name = subs.fn
+          print("Calling   " .. func_name 
+            .. " src_start = " .. src_start[0] 
+            .. " dst_start = " .. dst_start[0])
+          local status = qc[func_name](
+            sv_ptr, sl_ptr, src_start, sl_len, dl_ptr, dv_ptr, nn_dv_ptr, 
+            dst_start, dl_len)
           assert(status == 0)
-      end
+          print("Done with " .. func_name 
+            .. " src_start = " .. src_start[0] 
+            .. " src_len = " .. sl_len 
+            .. " dst_start = " .. dst_start[0] 
+            .. " dst_len = " .. dl_len )
+        end
+        -- unget the source, we may end up getting same chunk again 
+        print("SRC: Ungetting " .. src_chunk_num)
+        src_val:unget_chunk(src_chunk_num)
+        src_lnk:unget_chunk(src_chunk_num)
+        -- handle case when you need to get next chunk of source
+        if ( sl_len > 0 and src_start[0] == sl_len ) then 
+          -- source buffer consumed 
+          if ( sl_len == subs.max_num_in_chunk ) then 
+            print("SRC: Getting ready for next chunk")
+            src_chunk_num = src_chunk_num + 1
+            src_start[0] = 0
+          else
+            print("Will continue working on same src chunk")
+          end
+        end
+        if ( dst_start[0] == dl_len ) then
+          print("Breaking while loop")
+          break 
+        end
+        iter = iter + 1 
+      end -- end while loop AA
+      -- you have produced a chunk of the output, time to return it
       for _, join_type in ipairs(join_types) do 
         if ( join_type ~= my_join_type ) then
           vectors[join_type]:put_chunk(
@@ -123,16 +156,9 @@ local function expander_join(
             nn_dv_bufs[join_type])
         end 
       end
-      -- handle case when src buffer is over and you need to get new one
-      if ( sl_len > 0 and src_start[0] == sl_len ) then 
-        -- source buffer consumed 
-        print("SRC: Ungetting " .. src_chunk_num)
-        src_val:unget_chunk(src_chunk_num)
-        src_lnk:unget_chunk(src_chunk_num)
-        src_chunk_num = src_chunk_num + 1
-        src_start[0] = 0
-      end
       dst_lnk:unget_chunk(l_chunk_num)
+      print("DST: Ungetting " .. l_chunk_num)
+      l_chunk_num = l_chunk_num + 1 
       return dl_len, dv_bufs[my_join_type], nn_dv_bufs[my_join_type]
         --===================================================
     end
--- OPERATORS/JOIN/test/test_join.lua
+++ OPERATORS/JOIN/test/test_join.lua
@@ -47,5 +47,78 @@ tests.t1 = function ()
   end
   print("Test t1 succeeded")
 end
+tests.t2 = function()
+  -- we repeat this test twice, swapping source and destination 2nd time 
+  local M = {}
+  local O = { is_hdr = true, max_num_in_chunk = 64  }
+  M[#M+1] = { name = "id", qtype = "I4", has_nulls = false, }
+  M[#M+1] = { name = "lnk", qtype = "I4", has_nulls = false, }
+  M[#M+1] = { name = "val", qtype = "I4", has_nulls = false, }
+  for _, iter in ipairs({2, 1}) do 
+    print("TEST Iteration " .. iter)
+    -- load source data 
+    local datafile 
+    if ( iter == 1 ) then 
+      datafile = qcfg.q_src_root ..  "/OPERATORS/JOIN/test/join_src_1.csv"
+    else
+      datafile = qcfg.q_src_root ..  "/OPERATORS/JOIN/test/join_dst_1.csv"
+    end
+    assert(plpath.isfile(datafile))
+    local Tsrc = Q.load_csv(datafile, M, O)
+    assert(Tsrc.id:max_num_in_chunk() == 64)
+    -- load destination data 
+    if ( iter == 1 ) then  
+      datafile = qcfg.q_src_root ..  "/OPERATORS/JOIN/test/join_dst_1.csv"
+    else
+      datafile = qcfg.q_src_root ..  "/OPERATORS/JOIN/test/join_src_1.csv"
+    end
+    assert(plpath.isfile(datafile))
+    local Tdst = Q.load_csv(datafile, M, O)
+    assert(Tdst.id:max_num_in_chunk() == 64)
+    --========================================
+    local T = Q.join(Tsrc.val, Tsrc.lnk, Tdst.lnk)
+    assert(type(T) == "table")
+    local dv_val = assert(T.val)
+    assert(type(dv_val) == "lVector")
+    assert(dv_val:qtype() == Tsrc.val:qtype())
+    assert(dv_val:has_nulls())
+    assert(dv_val:max_num_in_chunk() == Tsrc.val:max_num_in_chunk())
+  
+    dv_val:eval()
+    assert(dv_val:check())
+    assert(Tsrc.lnk:check())
+    assert(Tsrc.val:check())
+    assert(Tdst.lnk:check())
+    assert(dv_val:check())
+    assert(dv_val:has_nulls())
+  
+    -- check contents of dv_val 
+  
+    dv_val:drop_nulls() -- needed for vveq to work 
+    if ( iter == 1 ) then 
+      local n1, n2 = Q.sum(Q.vveq(dv_val, Tdst.val)):eval()
+      assert(n1 == n2)
+    else
+      local chk_dv_val = Q.mk_col({10, 20, 30, 40, 50, 0, }, "I4",
+        { name = "chk_dv_val", max_num_in_chunk = 64}, 
+        { true, true, true, true, true, false, })
+      chk_dv_val:drop_nulls() -- needed for vveq to work 
+      local n1, n2 = Q.sum(Q.vveq(dv_val, chk_dv_val)):eval()
+      -- dv_val:pr()
+      -- print(n1, n2)
+      assert(n1 == n2)
+    end
+  
+    Tsrc.lnk:delete()
+    Tsrc.val:delete()
+    Tdst.lnk:delete()
+    dv_val:delete()
+    cVector.check_all()
+  
+    print("Test t2 succeeded for iteration " .. iter )
+  end
+  print("Test t2 succeeded")
+end
 tests.t1()
+tests.t2()
 -- return tests
